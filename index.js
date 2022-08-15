427a94ae4b1b492951bccccdf18344318b4e0859
Minchul An <min@minchulan.io>
2022-08-14 22:15:17 -0400

    commit tree ab7fea0b46bb88cf32a6845842e98f49521213c8

--- index.js
+++ index.js
@@ -1,2 +1,44 @@
 
-// Write your code here
+// describe('index.js', function() {
+//     describe('multiply', function() {
+//       it("is an equation whose multiplied results will equal 62", function() {
+//         expect(num1).to.be.a('number')
+//         expect(num2).to.be.a('number')
+//         expect(multiply).to.eq(62)
+//       });
+//     });
+
+
+// function () {
+//     let num1 = 31;
+//     let num2 = 2;
+//     return num1 * num2;
+// }
+
+let num1 = 31;
+let num2 = 2;
+let multiply = num1 * num2;
+
+
+// describe('random', function() {
+//     it("generates a random integer greater than 0", function() {
+//       expect(Number.isInteger(random)).to.be.true
+//       expect(random).to.not.eq(0)
+//     });
+//   });
+
+
+const random = 2 * 5;
+
+console.log(Number.isInteger(random));
+
+
+let num3 = 18;
+let num4 = 7;
+
+const mod = num3 % num4;
+
+
+const max = (1,5,10,20);
+console.log(Math.max);
+
